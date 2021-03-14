03c4b43c32a3006db987ad0a9c2db63f30e30a3d
Jon Bailey <jonbailey788@gmail.com>
2021-03-14 14:31:09 -0500

    card play button and hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import {ReactComponent as PlayIcon} from '../images/play-icon.svg';
 
 const Card = () => {
     return(
     <div className="cardWrapper">
      <div className="card">
         <div className="cardImage">
+        <span className="playButton">
+                <PlayIcon className="playIcon" />
+            </span>
             <img src="https://images.unsplash.com/photo-1615114814213-a245ffc79e9a?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="album-artwork" />
         </div>
         <div className="cardText">
             <h3>Liked Songs</h3>
-            <p></p>
+            <p>A playlist of your favorite songs</p>
         </div>
      </div>
   </div>
